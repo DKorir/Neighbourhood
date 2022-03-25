518329d76340a41f396c0dd4a3bef49d85c0fd86
DKorir <dominic.korir@student.moringaschool.com>
2022-03-25 20:19:00 +0300

    working on homepage

--- neighbourapp/views.py
+++ neighbourapp/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render,redirect
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
