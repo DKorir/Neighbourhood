c477592c0eb57616896e5df327c264b7fa5b8496
DKorir <dominic.korir@student.moringaschool.com>
2022-03-26 07:33:11 +0300

    running tests

--- neighbourapp/tests.py
+++ neighbourapp/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
-# Create your tests here.
+from django.test import TestCase
+from .models import Post,Neighbourhood,Profile,User
+class ProfileTestClass(TestCase):
+    #setup method
+    def setUp(self):
+        self.user = Profile(user='Levy')
+        self.user.save()
+        self.user_profile = Profile(user=self.user, profile_picture="image.png", bio="My bio")
+    def tearDown(self):
+        Profile.objects.all().delete()
